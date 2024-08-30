41a1dc31692e27977e36b2b583f983df06328378
GoldAnimate <mrmana341@gmail.com>
2024-08-29 19:09:56 -0700

    Update r.js

--- r.js
+++ r.js
@@ -5,4 +5,4 @@ bs.insertAdjacentHTML('afterend','<a class="button_big" href="http://wrapperonli
 ss.insertAdjacentHTML('afterend','<a class="button_small" href="http://wrapperonlinediamond.waterdroprio.cc/create?filter=cc">DIAMOND CC</a>');
 warning.style.backgroundColor="red";
 warning.style.color="white";
-warning.innerHTML='<b><a href=\'http://wrapperonlinediamond.waterdroprio.cc/\' style=\'color:white;\'>Wrapper Online: Diamond is highly recomended instead of '+window.location.hostname+'. Click here to visit Wrapper Online: Diamond now. (Highly Recomended)</a></b>';
+warning.innerHTML='<br><br><b><a href=\'http://wrapperonlinediamond.waterdroprio.cc/\' style=\'color:white;\'>Wrapper Online: Diamond is highly recomended instead of '+window.location.hostname+'. Click here to visit Wrapper Online: Diamond now. (Highly Recomended)</a></b>';
